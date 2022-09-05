142e17cf7f722334fd19052a0318342deeff90f4
Fadlun Anaturdasa <f.anaturdasa@gmail.com>
2022-09-05 07:54:48 +0700

    feat(settings): biometric prompt

--- src/views/Settings/SettingsView.tsx
+++ src/views/Settings/SettingsView.tsx
@@ -1,26 +1,45 @@
 import React from "react"
-import { Text, View, Pressable } from "react-native"
+import { Text, View, Pressable, Alert } from "react-native"
+import ReactNativeBiometrics from 'react-native-biometrics'
 import { NativeStackNavigationProp } from "@react-navigation/native-stack"
 import { useNavigation } from "@react-navigation/native"
 
 export default function SettingsView() {
   const navigation = useNavigation<NativeStackNavigationProp<any>>()
 
+  const handleBiometricID = () => {
+    const rnBiometrics = new ReactNativeBiometrics()
+
+    rnBiometrics.simplePrompt({promptMessage: 'Confirm Biometric ID'}).then((result) => {
+      const { success } = result
+
+      if (success) {
+        console.log('successful biometrics provided')
+      } else {
+        console.log('user cancelled biometric prompt')
+      }
+      
+    })
+    .catch(() => {
+      Alert.alert('Please setup your Finger/Face ID on your ponsel settings')
+    })
+  }
+
   return (
     <React.Fragment>
       <View>
         <Pressable 
           style={{ marginTop: 32, padding: 16, backgroundColor: 'gold' }}
-          onPress={() => navigation.navigate('FingerID')}
+          onPress={handleBiometricID}
         >
-          <Text>Finger ID</Text>
+          <Text>Biometric ID</Text>
         </Pressable>
-        <Pressable 
+        {/* <Pressable 
           style={{ marginTop: 8, padding: 16, backgroundColor: 'gold' }}
           onPress={() => navigation.navigate('FaceID')}
         >
           <Text>Face ID</Text>
-        </Pressable>
+        </Pressable> */}
       </View>
     </React.Fragment>
   )
